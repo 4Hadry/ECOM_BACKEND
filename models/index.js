1303215bc27444e9cf920599704d5f2d6533a9cd
4Hadry <chammar234@gmail.com>
2023-11-19 12:07:41 +0500

    Relation Created

--- models/index.js
+++ models/index.js
@@ -4,22 +4,26 @@ const category = require("./Schemas/categaryScema");
 const products = require("./Schemas/productSchema");
 const user = require("./Schemas/userSchema");
 
-// user.hasMany(cart, {
-//   onDelete: "CASCADE",
-//   foreignKey: {
-//     name: "userID",
-//     allowNull: false,
-//     unique: true,
-//   },
-// });
-// cart.belongsTo(user, {
-//   onDelete: "CASCADE",
-//   foreignKey: {
-//     name: "userID",
-//     allowNull: false,
-//     unique: true,
-//   },
-// });
+user.hasMany(cart, { onDelete: "CASCADE", foreignKey: { name: "userID" } });
+cart.belongsTo(user, { onDelete: "CASCADE", foreignKey: { name: "userID" } });
+
+category.hasMany(products, {
+  onDelete: "CASCADE",
+  foreignKey: { name: "categoryID" },
+});
+products.belongsTo(category, {
+  onDelete: "CASCADE",
+  foreignKey: { name: "categoryID" },
+});
+
+products.belongsToMany(cart, {
+  through: "product_cart",
+  foreignKey: { name: "productID" },
+});
+cart.belongsToMany(products, {
+  through: "product_cart",
+  foreignKey: { name: "cartID" },
+});
 const models = database.models;
 
 module.exports = { database, models };
